ac8f7a859e0241158c0f2777fbd3e80962670ec1
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-03-24 15:11:19 -0600

    adds class method to user to save to user db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
-class User < ApplicationRecord
+class User < ApplicationRecord::Base
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
 end
