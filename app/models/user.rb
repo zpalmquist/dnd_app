19db2957fb79a776a8f1861fceb8e17f8e4ffdda
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-03-24 15:24:42 -0600

    hitting errors when testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
-class User < ApplicationRecord::Base
+class User < ActiveRecord::Base
   def self.from_omniauth(auth)
+    byebug
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
