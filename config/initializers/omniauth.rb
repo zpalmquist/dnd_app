0ba4b5db2ef1a1c75af8fd9de448db71df762042
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-03-24 14:58:25 -0600

    adds google oauth cert files and permissions

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, 'my Google client id', 'my Google client secret', {client_option: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '8965533390-pb0b4os1ejeptkjcbnli17c95nke02ci.apps.googleusercontent.com', '9DWjSYfAYLrQHzXM-MfTId9o', {client_option: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
