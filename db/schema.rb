2430fd8be5e0e6166ab1a7b1259e341dd9ec0b00
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-04-01 00:30:21 -0600

    add user id to character db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180401062125) do
+ActiveRecord::Schema.define(version: 20180401062910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20180401062125) do
     t.integer "speed"
     t.integer "passive_perception"
     t.integer "initiative"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_characters_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
