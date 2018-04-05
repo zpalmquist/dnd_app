7ba3557359aa4126406267581c9d8d490e642cdb
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-04-04 18:37:12 -0600

    delete erroneous migrations

--- db/migrate/20180405002830_change_character_id_to_be_optional_for_races.rb
+++
@@ -1,5 +0,0 @@
-class ChangeCharacterIdToBeOptionalForRaces < ActiveRecord::Migration[5.1]
-  def change
-    change_column :races, :character_id, :integer, null: true
-  end
-end
--- db/migrate/20180405003045_change_character_id_to_be_optional_for_character_classes.rb
+++
@@ -1,5 +0,0 @@
-class ChangeCharacterIdToBeOptionalForCharacterClasses < ActiveRecord::Migration[5.1]
-  def change
-    change_column :character_classes, :character_id, :integer, null: true
-  end
-end
