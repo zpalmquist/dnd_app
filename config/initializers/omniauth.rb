831546fa89172ea00f2ee31146be4cb7e35a4b84
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-04-07 20:09:07 -0600

    remove commented out code

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
-# OmniAuth.config.logger = Rails.logger
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
   ENV["GOOGLE_CLIENT_ID"],
@@ -7,16 +5,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     {
       callback_path: '/auth/google_oauth2/callback'
     }
-  # {
-  #   name: "google",
-  #   scope: [''],
-  #   # ['contacts', 'plus.login', 'plus.me', 'email', 'profile'],
-  #   prompt: "select_account",
-  #   image_aspect_ratio: "square",
-  #   image_size: 50,
-  #   access_type: 'offline'
-  # }
-
-  # {client_option: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}},
-  #  :skip_jwt => true}
 end
